5a253412a2f0351a2b591a3d4ec586e5f5c7e949
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-10 16:59:28 -0300

    Update dag_main.py

--- dags/dag_main.py
+++ dags/dag_main.py
@@ -25,9 +25,6 @@ with DAG('dag_main', default_args=default_args, description='DAG to trigger a La
     trigger_lambda = TriggerLambdaOperator(
         task_id='trigger_lambda_task',
         lambda_function_name='hello_world'
-        # aws_access_key_id='secret',
-        # aws_secret_access_key='key-secret',
-        # aws_region_name='us-east-1'
     )
 
     ( trigger_lambda )
