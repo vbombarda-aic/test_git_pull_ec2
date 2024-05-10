6142fcaf86e5601b78675b9a4d51f0388c3488cb
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-10 17:35:07 -0300

    Update function_lambda_trigger.py

--- dags/function_lambda_trigger.py
+++ dags/function_lambda_trigger.py
@@ -9,15 +9,11 @@ class TriggerLambdaOperator(BaseOperator):
                  aws_region_name='us-east-1', *args, **kwargs):
         super(TriggerLambdaOperator, self).__init__(*args, **kwargs)
         self.lambda_function_name = lambda_function_name
-        # self.aws_access_key_id = aws_access_key_id
-        # self.aws_secret_access_key = aws_secret_access_key
         self.aws_region_name = aws_region_name
 
     def execute(self, context):
         # Create a Lambda client
         client = boto3.client('lambda',
-                              # aws_access_key_id=self.aws_access_key_id,
-                              # aws_secret_access_key=self.aws_secret_access_key,
                               region_name=self.aws_region_name)
         
         # Trigger the Lambda function
