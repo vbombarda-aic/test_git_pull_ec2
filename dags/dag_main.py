d27fa84de2f092c41acb642269938e6303dc64e0
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-13 10:03:25 -0300

    Update dag_main.py

--- dags/dag_main.py
+++ dags/dag_main.py
@@ -21,7 +21,7 @@ default_args = {
 with DAG('dag_main', default_args=default_args, description='DAG to trigger a Lambda function', schedule_interval='@daily',
                     start_date=datetime(2024, 5, 1), catchup=False) as dag:
 
-    payload = {"bucket_name:" "aws-bix-aicollaborator", "file_path": "template_example.csv"}
+    payload = {"bucket_name": "aws-bix-aicollaborator", "file_path": "template_example.csv"}
     trigger_lambda = TriggerLambdaOperator(
         task_id='trigger_lambda_task',
         lambda_function_name='hello_world',
