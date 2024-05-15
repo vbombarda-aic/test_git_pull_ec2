504f90ca772ee9853277deb1efa74c25d5b739b0
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-15 16:27:11 -0300

    Create db_start.py

---
+++ python_scripts/db_start.py
@@ -0,0 +1,19 @@
+import psycopg2
+from psycopg2 import sql
+
+conn = psycopg2.connect(
+            host="db-postgres-aic-instance.cx82qoiqyhd2.us-east-1.rds.amazonaws.com",
+            user="test_admin",
+            password="test_password",
+            port=5432)
+
+conn.autocommit = True 
+cursor = conn.cursor()
+print("Connection successful")
+
+db_name = "structured"
+try:
+cursor.execute(sql.SQL(f"CREATE DATABASE {db_name}"))
+  print(f"{db_name} successfully created!")
+except:
+  print("Error creating {db_name} database.")
