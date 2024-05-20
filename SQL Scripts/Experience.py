d5d3eeeb13f7d7a80eb735a6984218ab421cda8e
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-20 14:49:55 -0300

    Update Experience.py

--- SQL Scripts/Experience.py
+++ SQL Scripts/Experience.py
@@ -8,18 +8,14 @@ conn = psycopg2.connect(
             port=5432)
 
 cur = conn.cursor()
+
 cur.execute('''
-
-
 MERGE INTO
     Experience AS A
 USING (
-    SELECT DISTINCT "RespondentID",
-
-    TO_TIMESTAMP("Timestamp" / 1000) AS Timestamp,
-
-    "Survey", "Content",
-            CONCAT_WS('_', "RespondentID", "Timestamp", "Survey", "Content") AS mergeKey
+    SELECT DISTINCT "RespondentID", TO_TIMESTAMP("Timestamp", 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AS Timestamp,
+            "Survey", "Content",
+            CONCAT_WS('_', "RespondentID", TO_TIMESTAMP("Timestamp", 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AT TIME ZONE 'UTC', "Survey", "Content") AS mergeKey
 
     FROM temporary_table
 
@@ -30,9 +26,6 @@ ON CONCAT_WS('_', A.RespondentID, A.Timestamp, A.Survey, A.Content) = B.mergeKey
 WHEN NOT MATCHED
 THEN INSERT ("respondentid", "timestamp", "survey", "content")
 VALUES (B."RespondentID", b."timestamp", B."Survey", B."Content");
-
-
-
 ''')
 conn.commit()
 cur.close()
