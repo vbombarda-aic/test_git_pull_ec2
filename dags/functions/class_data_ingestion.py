397328ce19340a86ebaca9c8585418b8811a40db
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-21 06:09:53 -0300

    Update class_data_ingestion.py

--- dags/functions/class_data_ingestion.py
+++ dags/functions/class_data_ingestion.py
@@ -1,11 +1,9 @@
+from function_data_ingestion import get_data, create_table_and_insert_data
 from sqlalchemy import create_engine, text
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 import json
-import boto3
 import pandas as pd
-from io import StringIO
-import psycopg2
 
 # Database connection details
 DB_HOST = 'db-postgres-aic-instance.cx82qoiqyhd2.us-east-1.rds.amazonaws.com'
@@ -20,20 +18,6 @@ DateTime = "Timestamp"
 ContentID = "Content"
 Survey = "Survey"
 
-def get_data(bucket_name, file_key):
-    session = boto3.Session()
-    s3 = session.client('s3')
-    print('bucket_name ', bucket_name)
-    print('file_key ', file_key)
-    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
-    data = obj['Body'].read().decode('utf-8')
-    df = pd.read_csv(StringIO(data))
-    return df
-
-def create_table_and_insert_data(df, engine, table_name):
-    with engine.connect() as connection:
-        df.head(0).to_sql(name=table_name, con=engine, index=False, if_exists='replace')
-        df.to_sql(name=table_name, con=engine, index=False, if_exists='append')
 
 class InsertStructuredData(BaseOperator):
 
