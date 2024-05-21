ec475b0d34c849adcfc3a1bb37034fd993f083b7
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-21 06:09:12 -0300

    Update function_data_ingestion.py

--- dags/functions/function_data_ingestion.py
+++ dags/functions/function_data_ingestion.py
@@ -3,9 +3,11 @@ import pandas as pd
 from io import StringIO
 import psycopg2
 
-def read_csv_from_s3(bucket_name, file_key):
+def get_data(bucket_name, file_key):
     session = boto3.Session()
     s3 = session.client('s3')
+    print('bucket_name ', bucket_name)
+    print('file_key ', file_key)
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
     data = obj['Body'].read().decode('utf-8')
     df = pd.read_csv(StringIO(data))
@@ -15,4 +17,3 @@ def create_table_and_insert_data(df, engine, table_name):
     with engine.connect() as connection:
         df.head(0).to_sql(name=table_name, con=engine, index=False, if_exists='replace')
         df.to_sql(name=table_name, con=engine, index=False, if_exists='append')
-
