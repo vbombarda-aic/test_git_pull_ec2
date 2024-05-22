18d6207e2dd26d963aea1ac47621d44eeb8e577f
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-22 13:40:09 -0300

    Update function_data_ingestion.py

--- dags/functions/function_data_ingestion.py
+++ dags/functions/function_data_ingestion.py
@@ -13,7 +13,47 @@ def get_data(bucket_name, file_key):
     df = pd.read_csv(StringIO(data))
     return df
 
+
 def create_table_and_insert_data(df, engine, table_name):
     with engine.connect() as connection:
         df.head(0).to_sql(name=table_name, con=engine, index=False, if_exists='replace')
         df.to_sql(name=table_name, con=engine, index=False, if_exists='append')
+
+
+def transform_dict(data: dict or list, data_id, data_name: str, valueColumns: list = [], arrayColumns:list = []):
+    if type(data) == dict:
+        # Selects only the interested Columns
+        data = {key: data[key] for key in valueColumns + arrayColumns}
+        
+        # Transform dictionries into lists
+        for column in arrayColumns:
+            try:
+                data[column] = [x["name"] for x in data[column]]
+            except:
+                data[column] = [x["description"] for x in data[column]]
+        
+        data['id'] = data_id
+        data['data_name'] = data_name
+        return [data]
+    
+    elif type(data) == list:
+        result_list = []
+        for review in data:
+            # Selects only the interested Columns
+            final_dict = {key: review[key] for key in valueColumns + arrayColumns}
+            
+            # Transform dictionries into lists
+            for column in arrayColumns:
+                try:
+                    final_dict[column] = [x["name"] for x in final_dict[column]]
+                except:
+                    final_dict[column] = [x["description"] for x in final_dict[column]]
+            
+            final_dict['id'] = data_id
+            final_dict['name'] = data_name
+            result_list.append(final_dict)
+        return result_list
+        
+    else:
+        raise Exception("Data object has to be of type list or dict.")
+
