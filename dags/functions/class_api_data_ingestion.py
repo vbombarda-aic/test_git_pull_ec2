0cbe56346df0963ea7f5f0d4ec016173c914b4a5
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-23 08:34:16 -0300

    Update class_api_data_ingestion.py

--- dags/functions/class_api_data_ingestion.py
+++ dags/functions/class_api_data_ingestion.py
@@ -1,4 +1,4 @@
-from .function_data_ingestion import transform_dict
+from .function_data_ingestion import transform_dict, create_script_table
 from sqlalchemy import create_engine, text
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -7,13 +7,13 @@ import boto3
 import pandas as pd
 from io import StringIO
 
-def create_table_and_insert_data(df, engine, table_name): #, dtype):
+def table_insert_data(df, engine, table_name): #, dtype):
     with engine.connect() as connection:
         #try:
           #df.head(0).to_sql(name=table_name, con=engine, index=False, if_exists='fail') #, dtype=dtype)
         #except:
           #pass
-        df.head(0).to_sql(name=table_name, con=engine, index=False, if_exists='replace')
+        # df.head(0).to_sql(name=table_name, con=engine, index=False, if_exists='replace')
         df.to_sql(name=table_name, con=engine, index=False, if_exists='append')
 
 def get_data(bucket_name, file_key):
@@ -43,17 +43,20 @@ class InsertApiData(BaseOperator):
       file_path_split = self.file_path + 'review_and_details/' + str(execution_date) + '/1/content.json' # ITERATES THROUGH THE IDs IN THE 1 ! 
       data = get_data(self.bucket_name, file_path_split)
       data = json.loads(data)
+      
 
       # Open Critic Info
       valueColumns = ['percentRecommended', 'numReviews', 'medianScore', 'topCriticScore','tier', 'description']
       arrayColumns = ['Companies', 'Genres']
       data_oc_info = transform_dict(data['oc_info'], data['id'], data['name'], valueColumns=valueColumns, arrayColumns=arrayColumns)
-      df_oc_info = pd.DataFrame(data_oc_info)  
+      df_oc_info = pd.DataFrame(data_oc_info)
+      sql_oc_info = create_script_table("openCriticInfo", valueColumns, arrayColumns)
         
       # Open Critic Reviews
       valueColumns = ['score', 'language', 'publishedDate', 'snippet', 'externalUrl']
       data_oc_reviews = transform_dict(data['oc_reviews'], data['id'], data['name'], valueColumns=valueColumns)
-      df_oc_reviews = pd.DataFrame(data_oc_reviews)  
+      df_oc_reviews = pd.DataFrame(data_oc_reviews)
+      sql_oc_reviews = create_script_table("openCriticReviews", valueColumns, arrayColumns)
 
       # Steam Info
       valueColumns = ['short_description']
@@ -62,11 +65,13 @@ class InsertApiData(BaseOperator):
       data_steam_info = transform_dict(data['steam_info'][game_id]['data'], data['id'], data['name'],
                              valueColumns=valueColumns, arrayColumns=arrayColumns)
       df_steam_info= pd.DataFrame(data_steam_info)
+      sql_steam_info = create_script_table("steamInfo", valueColumns, arrayColumns)
     
       # Steam Reviews
       valueColumns = ['language', 'review', 'voted_up','votes_up','votes_funny', 'timestamp_created', 'timestamp_updated']
       data_steam_reviews = transform_dict(data['steam_reviews']['reviews'], data['id'], data['name'], valueColumns=valueColumns)
       df_steam_reviews = pd.DataFrame(data_steam_reviews)
+      sql_steam_reviews = create_script_table("steamReviews", valueColumns, arrayColumns)
 
       ### FINISH
         
@@ -82,7 +87,9 @@ class InsertApiData(BaseOperator):
       engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
       print("engine created")
-      create_table_and_insert_data(df_steam_info, engine, "steamInfo")
+      with engine.connect() as con:
+          con.execute(text(sql_steam_info))
+      table_insert_data(df_steam_info, engine, "steamInfo")
       print("table created")
 
       return True
