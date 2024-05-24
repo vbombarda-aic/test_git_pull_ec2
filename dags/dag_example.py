caadab35192c203ed5b1024550eb6a98d22faf71
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-24 09:33:01 -0300

    Update dag_example.py

--- dags/dag_example.py
+++ dags/dag_example.py
@@ -2,6 +2,16 @@ from airflow import DAG
 from datetime import date, timedelta, datetime
 from airflow.operators.python import PythonOperator
 from airflow.models.baseoperator import BaseOperator
+from functions.class_mapping_ingestion import InsertApiData
+
+# Database connection details
+db_credentials = {
+"DB_HOST":     'db-postgres-aic-instance.cx82qoiqyhd2.us-east-1.rds.amazonaws.com',
+"DB_NAME":     'structured',
+"DB_USER":     'test_admin',
+"DB_PASSWORD": 'test_password',
+"DB_PORT":     '5432'
+}
 
 DAG_DEFAULT_ARGS = {
     'owner': 'airflow',
@@ -10,22 +20,16 @@ DAG_DEFAULT_ARGS = {
     'retry_delay': timedelta(minutes=1)
 }
 
-class HelloOperator(BaseOperator):
-    def __init__(self, name: str, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.name = name
-
-    def execute(self, context):
-        message = f"Hello {self.name}"
-        print(message)
-        return message
-
 # Dag is instanciated
-with DAG('dag_example', start_date=datetime(2018, 10, 1), schedule_interval='@daily', default_args = DAG_DEFAULT_ARGS, catchup = False):
+with DAG('dag_mapping', start_date=datetime(2018, 10, 1), schedule_interval='@daily', default_args = DAG_DEFAULT_ARGS,
+         catchup = False, is_paused_upon_creation=):
 
-  task1 = HelloOperator(
-    task_id="dag_example",
-    name="Airflow"
+  task1 = InsertApiData(
+    task_id="data_mapping_insertion",
+    bucket_name = 'argo-data-lake',
+    file_path = 'mapping/questions_categories_mapping.csv',
+    table_name = 'mapping_table',
+    db_credentials = db_credentials
   )
   
   (task1)
