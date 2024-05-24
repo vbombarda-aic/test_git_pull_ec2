1bce07f9ffbf5bb22f35958dc5cd0e50dc2bb11e
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-24 11:13:28 -0300

    Update dag_mapping.py

--- dags/dag_mapping.py
+++ dags/dag_mapping.py
@@ -3,6 +3,7 @@ from datetime import date, timedelta, datetime
 from airflow.operators.python import PythonOperator
 from airflow.models.baseoperator import BaseOperator
 from functions.class_mapping_ingestion import InsertApiData
+from functions.class_query_db import PostgresQueryOperator
 
 # Database connection details
 db_credentials = {
@@ -20,9 +21,17 @@ DAG_DEFAULT_ARGS = {
     'retry_delay': timedelta(minutes=1)
 }
 
+def get_sql_file(file_name):
+    sql_file_path = os.path.join(dag_directory, f'sql/{file_name}.sql')
+    with open(sql_file_path, 'r') as file:
+        return file.read()
+
+update_sql = get_sql_file("update_questions_description.sql")
+create_categories_table_sql = get_sql_file("create_categories_table_sql.sql")
+
 # Dag is instanciated
-with DAG('dag_mapping', start_date=datetime(2018, 10, 1), schedule_interval='@daily', default_args = DAG_DEFAULT_ARGS,
-         catchup = False, is_paused_upon_creation=False):
+with DAG('dag_mapping', start_date=datetime(2018, 10, 1), schedule_interval=None, is_paused_upon_creation=False, default_args = DAG_DEFAULT_ARGS,
+         catchup = False):
 
   task1 = InsertApiData(
     task_id="data_mapping_insertion",
@@ -30,7 +39,17 @@ with DAG('dag_mapping', start_date=datetime(2018, 10, 1), schedule_interval='@da
     file_path = 'mapping/questions_categories_mapping.csv',
     table_name = 'mapping_table',
     db_credentials = db_credentials
+  ) 
+  task2 = PostgresQueryOperator(
+        task_id='update_questions_description',
+        sql_query=update_sql,
+        db_credentials=db_credentials
+  )
+  task3 = PostgresQueryOperator(
+        task_id='create_categories_table',
+        sql_query=create_categories_table_sql,
+        db_credentials=db_credentials
   )
   
-  (task1)
+  (task1 >> [task2, task3])
   
