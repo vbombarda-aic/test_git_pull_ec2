b3eb180fd511badceea71092ebe18e1852462d64
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-26 20:34:00 -0300

    Update db_start.py

--- python_scripts/db_start.py
+++ python_scripts/db_start.py
@@ -12,9 +12,16 @@ conn.autocommit = True
 cursor = conn.cursor()
 print("Connection successful")
 
-db_name = "structured"
+db_structured = "structured"
+db_analytics = "analytics"
 try:
-  cursor.execute(sql.SQL(f"CREATE DATABASE {db_name}"))
-  print(f"{db_name} successfully created!")
+  cursor.execute(sql.SQL(f"CREATE DATABASE {db_structured}"))
+  print(f"{db_structured} successfully created!")
 except:
-  print("Error creating {db_name} database.")
+  print(f"Error creating {db_structured} database.")
+
+try:
+  cursor.execute(sql.SQL(f"CREATE DATABASE {db_analytics}"))
+  print(f"{db_analytics} successfully created!")
+except:
+  print(f"Error creating {db_analytics} database.")
