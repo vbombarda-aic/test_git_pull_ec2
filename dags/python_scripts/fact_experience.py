0a786ec696eefa43609abf605400407eb1779fc0
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-27 15:29:33 -0300

    Update fact_experience.py

--- dags/python_scripts/fact_experience.py
+++ dags/python_scripts/fact_experience.py
@@ -40,7 +40,7 @@ def create_fact_experience(db_credentials):
     columns = cur.fetchall()
     print('total of columns: ',len(columns))
     # Extract column names
-    column_names = [row[0] for row in columns]
+    column_names = [row[0].strip() + "_bool" for row in columns]
 
     # Create the column definition string for the crosstab query
     column_definitions2 = ", ".join([f'"{col}" TEXT' for col in column_names])
