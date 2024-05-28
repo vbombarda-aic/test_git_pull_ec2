4cb054b27d53f308b334bee5e71e5c4569a471a1
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-28 12:57:35 -0300

    Update create_categories_table_sql.sql

--- dags/sql/create_categories_table_sql.sql
+++ dags/sql/create_categories_table_sql.sql
@@ -1,7 +1,11 @@
 DROP TABLE IF EXISTS structured.QuestionsCategories;
 
-CREATE TABLE structured.QuestionsCategories AS
+CREATE TABLE structured.QuestionsCategories (
+  "Category" text,
+  "Question" text,
+  FOREIGN KEY ("Question") REFERENCES structured.SurveyQuestions("Question")
+);
 
+INSERT INTO structured.QuestionsCategories ("Category", "Question")
   SELECT A."Category", A."Question"
-
-  FROM "mapping_table" AS A
+  FROM "mapping_table" AS A;
