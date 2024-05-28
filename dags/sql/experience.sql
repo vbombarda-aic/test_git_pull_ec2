4831307f8a543caeb8a2610e7af17c96d77f20c3
vbombarda-aic <167908275+vbombarda-aic@users.noreply.github.com>
2024-05-28 14:15:18 -0300

    Update experience.sql

--- dags/sql/experience.sql
+++ dags/sql/experience.sql
@@ -2,21 +2,23 @@ MERGE INTO
     structured.Experience AS A
 USING (
     
-    SELECT DISTINCT "RespondentID",
-                    TO_TIMESTAMP("Timestamp", 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AS Timestamp,
-                    "Survey",
-                    "Content",
-                    CONCAT_WS('_', "RespondentID",
-                                   TO_TIMESTAMP("Timestamp", 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AT TIME ZONE 'UTC',
-                                   "Survey",
-                                   "Content") AS mergeKey
+    SELECT DISTINCT C."RespondentID",
+                    TO_TIMESTAMP(C."Timestamp", 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AS Timestamp,
+                    C."Survey",
+                    D."contentid",
+                    CONCAT_WS('_', C."RespondentID",
+                                   TO_TIMESTAMP(C."Timestamp", 'YYYY-MM-DD"T"HH24:MI:SS"Z"') AT TIME ZONE 'UTC',
+                                   C."Survey",
+                                   D."contentid") AS mergeKey
 
-    FROM temporary_table
+    FROM temporary_table C
+
+    LEFT JOIN structured.content D ON C."Content" = D."content"
 
 ) B
 
-ON CONCAT_WS('_', A.RespondentID, A.Timestamp, A.Survey, A.Content) = B.mergeKey
+ON CONCAT_WS('_', A.RespondentID, A.Timestamp, A.Survey, A.ContentID) = B.mergeKey
 
 WHEN NOT MATCHED
-THEN INSERT ("respondentid", "timestamp", "survey", "content")
-VALUES (B."RespondentID", b."timestamp", B."Survey", B."Content");
+THEN INSERT ("respondentid", "timestamp", "survey", "contentid")
+VALUES (B."RespondentID", b."timestamp", B."Survey", B."contentid");
